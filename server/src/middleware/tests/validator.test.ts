95491adecbf1692ececd0d963273411a9dd63680
이주현 <leejuhyun@ijuhyeons-MacBook-Pro.local>
2022-01-17 17:27:38 +0900

    TDD for validator and auth middleware

---
+++ server/src/middleware/tests/validator.test.ts
@@ -0,0 +1,50 @@
+import httpMocks from "node-mocks-http";
+import * as validator from "express-validator";
+import { validate } from "../validator";
+
+jest.mock('express-validator');
+
+describe('Validator Middleware', () => {
+  let request;
+  let response;
+  let next;
+
+  beforeEach(()=>{
+    request = httpMocks.createRequest();
+    response = httpMocks.createResponse();
+    next = jest.fn();
+  })  
+
+  it("calls next if there no validation errors", () => {
+    Object.defineProperty(validator, 'validationResult', {
+      value: jest.fn(() => ({ 
+        isEmpty: () => true }))
+      });
+    // validator.validationResult = jest.fn(() => ({ 
+    //   isEmpty: () => true 
+    // }));
+    
+    validate(request, response, next);
+    
+    expect(next).toBeCalled();
+  })
+  
+  it("returns 400 if there are validation errors", () => {
+    Object.defineProperty(validator, 'validationResult', {
+      value: jest.fn(() => ({ 
+        isEmpty: () => false, 
+        array: () => [{msg: 'Error!'}]
+      }))
+    });
+    // validator.validationResult = jest.fn(()=>({
+    //   isEmpty: () => false, 
+    //   array: () => [{msg: 'Error!'}]
+    // }));
+
+    validate(request, response, next);
+
+    expect(response.statusCode).toBe(400);
+    expect(next).not.toBeCalled();
+    expect(response._getJSONData().message).toBe("Error!")
+  })
+})
