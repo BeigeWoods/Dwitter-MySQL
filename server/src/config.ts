a5377382a2646a1fccced4b5cc4020f60d72ee0a
BeigeWoods <juhyunkbstar@gmail.com>
2023-01-12 20:23:32 +0900

    modifying required function type
    using generic with union type

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,15 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-// .env에서 키 값이 제대로 주어졌는지 확인한 후 키 값을 반환하는 함수
-function required(key: string, defaultValue?: number | string): string {
-  const value = process.env[key] || defaultValue;
-  if (value == null) {
-    throw new Error(`Key ${key} is undefined`);
-  }
-  return value as string;
-}
-
 export type Config = {
   cors: {
     allowedOrigin: string;
@@ -42,18 +33,30 @@ export type Config = {
   };
 };
 
+// .env에서 키 값이 제대로 주어졌는지 확인한 후 키 값을 반환하는 함수
+function required<V extends string | number>(
+  key: string,
+  defaultValue?: number
+): V {
+  const value = process.env[key] || defaultValue;
+  if (!value) {
+    throw new Error(`Key ${key} is undefined`);
+  }
+  return value as V;
+}
+
 export const config: Config = {
   cors: {
     allowedOrigin: required("ALLOWED_ORIGIN"),
   },
   jwt: {
     secretKey: required("JWT_SECRET"),
-    expiresInSec: parseInt(required("JWT_EXPIRES_SEC", 86400)),
+    expiresInSec: required("JWT_EXPIRES_SEC", 86400),
   },
   bcrypt: {
-    saltRounds: parseInt(required("BCRYPT_SALT_ROUNDS", 12)),
+    saltRounds: required("BCRYPT_SALT_ROUNDS", 12),
   },
-  port: parseInt(required("HOST_PORT", 8080)),
+  port: required("HOST_PORT", 8080),
   db: {
     host: required("DB_HOST"),
     user: required("DB_USER"),
