2589137536fdd34892d33449b1a83f882296586e
BeigeWoods <juhyunkbstar@gmail.com>
2023-03-19 22:16:12 +0900

    refactoring oauth file

--- server/src/__dwitter__.d.ts/controller/auth/oauth.d.ts
+++ server/src/__dwitter__.d.ts/controller/auth/oauth.d.ts
@@ -3,14 +3,19 @@ import { Config } from "../../config.js";
 import { TokenHandler } from "./token.js";
 import { UserDataHandler } from "../../data/auth.js";
 export interface GithubOauth {
-    githubStart(req: Request, res: Response): Promise<void>;
-    githubFinish(req: Request, res: Response): Promise<void>;
+  githubStart(req: Request, res: Response): Promise<void>;
+  githubFinish(req: Request, res: Response): Promise<void>;
 }
 export default class OauthController implements GithubOauth {
-    private config;
-    private tokenController;
-    private userRepository;
-    constructor(config: Config, tokenController: TokenHandler, userRepository: UserDataHandler);
-    githubStart: (req: Request, res: Response) => Promise<void>;
-    githubFinish: (req: Request, res: Response) => Promise<void>;
+  private config;
+  private tokenController;
+  private userRepository;
+  constructor(
+    config: Config,
+    tokenController: TokenHandler,
+    userRepository: UserDataHandler
+  );
+  githubStart: (req: Request, res: Response) => Promise<void>;
+  private setUser;
+  githubFinish: (req: Request, res: Response) => Promise<void>;
 }
