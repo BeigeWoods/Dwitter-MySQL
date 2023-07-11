ebd5fb30397f63a5c99579eee51f381eefc1545c
BeigeWoods <juhyunkbstar@gmail.com>
2023-07-11 23:18:59 +0900

    deploy 2

--- server/script/execute.sh
+++
@@ -1,7 +0,0 @@
-REPOSITORY=server
-
-cp ../.env ../$REPOSITORY/.env
-
-# sudo npm i
-
-sudo npm start
---
+++ server/script/start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+REPOS=/home/ubuntu
+
+cp $REPOS/.env $REPOS/server/.env
+
+sudo npm start
