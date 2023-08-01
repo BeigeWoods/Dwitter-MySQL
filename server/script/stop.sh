a55fb76d81f8c1753e358e25daef96f8e4d67b77
BeigeWoods <juhyunkbstar@gmail.com>
2023-08-01 23:01:36 +0900

    deploy34

--- server/script/stop.sh
+++ server/script/stop.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-PATH="/home/ubuntu/server/dist/app.js"
-CURRENT_PID=$(sudo pgrep -f $PATH)
+path="/home/ubuntu/server/dist/app.js"
+pid=$(pgrep -f $path)
 
-if [ -n $CURRENT_PID ]
+if [ -n $pid ]
 then
-  sudo pm2 stop $PATH
+  sudo pm2 stop $path
   echo "stopped pm2"
 else
   echo "skipped to stop pm2"
